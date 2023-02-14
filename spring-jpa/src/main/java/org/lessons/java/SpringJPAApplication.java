4225acf1fc6a00f451c5b90d27adaf38fb2d0f09
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-02-14 13:49:30 +0100

    primo commit

---
+++ spring-jpa/src/main/java/org/lessons/java/SpringJPAApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJPAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJPAApplication.class, args);
+	}
+
+}
